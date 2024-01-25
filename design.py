d85802ffbe070309c4e34b04f6d53a1cd3af0b60
mysticsaugat <152575469+mysticsaugat@users.noreply.github.com>
2024-01-25 22:44:53 +0545

    Create design.py

---
+++ design.py
@@ -0,0 +1,26 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+BG_COLOR = (1, 1, 1)
+LINE_COLOR = (66, 66, 66) 
+CIRC_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
